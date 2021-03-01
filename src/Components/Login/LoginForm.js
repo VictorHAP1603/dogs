e7cbdfb111ad807dafbce5223b7726557bf4977a
repicco <renato.picco@newpost.com.br>
2021-03-01 17:28:39 -0300

    08-12

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Error from '../Helper/Error'
 import Input from "../Forms/Input";
 import Button from "../Forms/Button";
 import useForm from "../../Hooks/useForm";
 import { UserContext } from "../../Context/userContext";
+
+import '../../css/Form/LoginForm.css'
+import '../../css/Form/Button.css'
 
 const LoginForm = () => {
   const username = useForm();
@@ -18,17 +22,20 @@ const LoginForm = () => {
   }
 
   return (
-    <section>
-      <h1>Login</h1>
-      <form action="" onSubmit={handleSubmit}>
+    <section className="animeLeft" >
+      <h1 className="title" >Login</h1>
+      <form className="form" onSubmit={handleSubmit}>
         <Input type="text" label="Usuário" {...username} />
         <Input type="password" label="Senha" {...password} />
         {loading ? <Button disabled value="Carregando..." /> : <Button value="Entrar" />}
-        {error && <p>{error}</p>}
+        <Error error={error} />
       </form>
-      <Link to="/login/cadastro">Cadastro</Link>
-      <Link to="/login/recuperarSenha">Recuperar senha</Link>
-      <Link to="/login/resetarSenha">Resetar a senha</Link>
+      <Link className="perdeu" to="/login/recuperarSenha">Perdeu a senha ?</Link>
+      <div className="cadastro">
+        <h2 className="subtitle" >Cadastre-se</h2>
+        <p>Ainda não possui conta ? Cadastre-se no site</p>
+        <Link className="button" to="/login/criar" >Cadastro</Link>
+      </div>
     </section>
   );
 };
