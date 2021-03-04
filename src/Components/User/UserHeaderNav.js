8edea7aad8f3f7f2ca151cc27d575b76b63eaa12
Victor Hugo <vitor.hugo@newpost.com>
2021-03-04 01:01:55 -0300

    adicionando menu mobile

--- src/Components/User/UserHeaderNav.js
+++ src/Components/User/UserHeaderNav.js
@@ -1,44 +1,66 @@
-import React from 'react'
+import React from "react";
 
-import { UserContext } from '../../Context/userContext'
-import { NavLink } from 'react-router-dom'
+import { UserContext } from "../../Context/userContext";
+import { NavLink, useLocation } from "react-router-dom";
 
-import {ReactComponent as MinhasFotos} from '../../Assets/feed.svg'
-import {ReactComponent as Estatisticas} from '../../Assets/estatisticas.svg'
-import {ReactComponent as Adicionar} from '../../Assets/adicionar.svg'
-import {ReactComponent as Sair} from '../../Assets/sair.svg'
+import { ReactComponent as MinhasFotos } from "../../Assets/feed.svg";
+import { ReactComponent as Estatisticas } from "../../Assets/estatisticas.svg";
+import { ReactComponent as Adicionar } from "../../Assets/adicionar.svg";
+import { ReactComponent as Sair } from "../../Assets/sair.svg";
 
-import styles from './UserHeaderNav.module.css'
+import styles from "./UserHeaderNav.module.css";
+import useMedia from "../../Hooks/useMedia";
 
 const UserHeaderNav = () => {
-    const [mobile, setMobile] = React.useState(null)
-    const { userLogout } = React.useContext(UserContext)
+  const [mobileMenu, setMobileMenu] = React.useState(false);
+  const { userLogout } = React.useContext(UserContext);
+  const mobile = useMedia("(max-width: 40rem)");
 
-    const { matches } = window.matchMedia('(max-width: 40rem)')
+  const { pathname } = useLocation();
 
-    return (
-        <nav className={styles.nav} >
-            <NavLink to="/minhaConta" end activeClassName={styles.active}>
-                <MinhasFotos />
-                {mobile && 'Minhas Fotos' }
-            </NavLink>
+  React.useEffect(() => {
+    setMobileMenu(false);
+  }, [pathname]);
 
-            <NavLink to="estatisticas" activeClassName={styles.active}>
-                <Estatisticas />
-                {mobile && 'Estatísticas'}
-            </NavLink>
+  return (
+    <>
+      {mobile && (
+        <button
+          className={`${styles.mobileButton} ${
+            mobileMenu && styles.mobileButtonActive
+          } `}
+          aria-label="Menu"
+          onClick={() => setMobileMenu(!mobileMenu)}
+        ></button>
+      )}
 
-            <NavLink to="postar" activeClassName={styles.active}>
-                <Adicionar />
-                {mobile && 'Adicionar Foto'}
-            </NavLink>
+      <nav
+        className={`${mobile ? styles.navMobile : styles.nav} user-header-nav ${
+          mobileMenu && styles.navMobileActive
+        }`}
+      >
+        <NavLink to="/minhaConta" end activeClassName={styles.active}>
+          <MinhasFotos />
+          {mobile && "Minhas Fotos"}
+        </NavLink>
 
-            <button onClick={userLogout} activeClassName={styles.active}>
-                <Sair />
-                {mobile && 'Sair'}
-            </button>
-        </nav>
-    )
-}
+        <NavLink to="estatisticas" activeClassName={styles.active}>
+          <Estatisticas />
+          {mobile && "Estatísticas"}
+        </NavLink>
 
-export default UserHeaderNav
+        <NavLink to="postar" activeClassName={styles.active}>
+          <Adicionar />
+          {mobile && "Adicionar Foto"}
+        </NavLink>
+
+        <button onClick={userLogout}>
+          <Sair />
+          {mobile && "Sair"}
+        </button>
+      </nav>
+    </>
+  );
+};
+
+export default UserHeaderNav;
--- src/Hooks/useMedia.js
+++ src/Hooks/useMedia.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const useMedia = (media) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  const [match, setMatch] = React.useState(null);
 
-export default useMedia
+  React.useEffect(() => {
+    function changeMatch() {
+      const { matches } = window.matchMedia(media);
+      setMatch(matches);
+    }
+    changeMatch();
+    window.addEventListener("resize", changeMatch);
+    return () => {
+      window.removeEventListener("resize", changeMatch);
+    };
+  }, [media]);
+
+  return match;
+};
+
+export default useMedia;
